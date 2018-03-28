546761989508baf781101f1c0cf021477e1b3a7d
ortiscz <tichypetr93@gmail.com>
2018-03-27 21:11:53 -0400

    Admin

---
+++ view/admin/partials/body_start.php
@@ -0,0 +1,35 @@
+<body>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-3" id="menu">				
+				<div class="row bg-grey">
+					<div class="col mt-4 mb-4">
+						<span class="user-name white">
+							Petr<br>
+							Tichy</span>
+					</div>
+				</div>
+				<ul class="nav nav-pills flex-column">
+				  <li class="nav-item">
+				    <a class="nav-link active" href="./index.php">Purchases</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="./members.php">Members</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="#">Theatres</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="#">Movies</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="#">Statistics</a>
+				  </li>
+				</ul> 				
+			</div>
+			<div class="col-9" id="content">
+				<div class="row">
+					<div class="col mt-4">
+						
+					
