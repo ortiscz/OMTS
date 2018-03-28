ade56b2b0491e2a9f2952629ec7a442fc75b2975
ortiscz <tichypetr93@gmail.com>
2018-03-27 22:22:19 -0400

    --clear

--- testDeleteShowing.php
+++
@@ -1,4 +0,0 @@
-<form class = "testing" action = "./scripts/AdminDeleteshowing.php" method = "post">
-    <input type="text" placeholder="Showing ID" class="form-control" name="showing-id">
-    <button type="submit" class="btn btn-primary btn-block mb-4 mt-4">Submit</button>
-</form>
--- testDeleteWeb.php
+++
@@ -1,5 +0,0 @@
-<form class = "testing" action = "./scripts/adminRemoveMember.php" method = "post">
-    <input type="email" placeholder="EMAIL" class="form-control" name="email">
-    <input type="text" placeholder="ACCOUNT NUMBER" class="form-control" name="accountNumber">
-    <button type="submit" class="btn btn-primary btn-block mb-4 mt-4">Submit</button>
-</form>
