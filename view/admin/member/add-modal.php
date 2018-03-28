cded51fd28838dd79ff16cd50816ffcc8c7444fe
ortiscz <tichypetr93@gmail.com>
2018-03-27 23:15:21 -0400

    Admin - Members - Add + Delete

--- view/admin/member/add-modal.php
+++ view/admin/member/add-modal.php
@@ -4,21 +4,21 @@
             <div class="modal-content container-dark-blue">
                 <!-- Modal body -->
                 <div class="modal-body">
-                    <a class="register-close white" data-dismiss="modal" href="javascript:void()">x</a>
+                    <a class="add-close white" data-dismiss="modal" href="javascript:void()">x</a>
                     <div class="continer">
                         <h1 class="modal-title text-center h3 blue mb-4 mt-2">Add new user!</h1>
 
-                        <form class="register-form" action="./scripts/register.php" method="post">
+                        <form class="add-form" action="" method="post">
                             <div class="form-group row">
-                                <label for="register-email" class="col-sm-4 col-form-label text-right white">Email address</label>
+                                <label for="add-email" class="col-sm-4 col-form-label text-right white">Email address</label>
                                 <div class="col-sm-8">
-                                    <input type="email" class="form-control" name="register-email" id="register-email">
+                                    <input type="email" class="form-control" name="add-email" id="add-email">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="register-password" class="col-sm-4 col-form-label text-right white">Password</label>
+                                <label for="add-password" class="col-sm-4 col-form-label text-right white">Password</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="register-password" id="register-password">
+                                    <input type="text" class="form-control" name="add-password" id="add-password">
                                 </div>
                             </div>
                             <div class="form-check row">
@@ -30,74 +30,74 @@
                             <hr>
                             <h2 class="text-center h6 blue mb-2">Detail information</h2>
                             <div class="form-group row">
-                                <label for="register-fname" class="col-sm-4 col-form-label text-right white">First name</label>
+                                <label for="add-fname" class="col-sm-4 col-form-label text-right white">First name</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="register-fname" id="register-fname">
+                                    <input type="text" class="form-control" name="add-fname" id="add-fname">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="register-lname" class="col-sm-4 col-form-label text-right white">Last name</label>
+                                <label for="add-lname" class="col-sm-4 col-form-label text-right white">Last name</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="register-lname" id="register-lname">
+                                    <input type="text" class="form-control" name="add-lname" id="add-lname">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="register-city" class="col-sm-4 col-form-label text-right white">City</label>
+                                <label for="add-city" class="col-sm-4 col-form-label text-right white">City</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="register-city">
+                                    <input type="text" class="form-control" name="add-city">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="register-streetnum" class="col-sm-4 col-form-label text-right white">Street number</label>
+                                <label for="add-streetnum" class="col-sm-4 col-form-label text-right white">Street number</label>
                                 <div class="col-sm-3">
-                                    <input type="text" class="form-control" name="register-streetnum" id="register-streetnum">
+                                    <input type="text" class="form-control" name="add-streetnum" id="add-streetnum">
                                 </div>                                
                             </div>
                             <div class="form-group row">
-                                <label for="register-streetname" class="col-sm-4 col-form-label text-right white">Street name</label>
+                                <label for="add-streetname" class="col-sm-4 col-form-label text-right white">Street name</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="register-streetname" id="register-streetname">
+                                    <input type="text" class="form-control" name="add-streetname" id="add-streetname">
                                 </div>                                
                             </div>
                             <div class="form-group row">
-                                <label for="register-state" class="col-sm-4 col-form-label text-right white">State</label>
+                                <label for="add-state" class="col-sm-4 col-form-label text-right white">State</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="register-state" id="register-state">
+                                    <input type="text" class="form-control" name="add-state" id="add-state">
                                 </div>                                
                             </div>
                             <div class="form-group row">
-                                <label for="register-zip" class="col-sm-4 col-form-label text-right white">ZIP</label>
+                                <label for="add-zip" class="col-sm-4 col-form-label text-right white">ZIP</label>
                                 <div class="col-sm-3">
-                                    <input type="text" class="form-control" name="register-zip" id="register-zip">
+                                    <input type="text" class="form-control" name="add-zip" id="add-zip">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="register-phone" class="col-sm-4 col-form-label text-right white">Phone</label>
+                                <label for="add-phone" class="col-sm-4 col-form-label text-right white">Phone</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="register-phone" id="register-phone">
+                                    <input type="text" class="form-control" name="add-phone" id="add-phone">
                                 </div>                                
                             </div>
                             <hr>
                             <h2 class="text-center h6 blue mb-2 blue">Card information</h2>
                             <div class="form-group row">
-                                <label for="register-card" class="col-sm-4 col-form-label text-right white">Card number</label>
+                                <label for="add-card" class="col-sm-4 col-form-label text-right white">Card number</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="register-card" id="register-card">
+                                    <input type="text" class="form-control" name="add-card" id="add-card">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="register-expiration" class="col-sm-4 col-form-label text-right white">Card expiration</label>
+                                <label for="add-expiration" class="col-sm-4 col-form-label text-right white">Card expiration</label>
                                 <div class="col-sm-3">
-                                    <input type="text" class="form-control" name="register-expiration" id="register-expiration" placeholder="MM/YY">
+                                    <input type="text" class="form-control" name="add-expiration" id="add-expiration" placeholder="MM/YY">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="register-ccv" class="col-sm-4 col-form-label text-right white">CCV</label>
+                                <label for="add-ccv" class="col-sm-4 col-form-label text-right white">CCV</label>
                                 <div class="col-sm-3">
-                                    <input type="text" class="form-control" name="register-ccv" id="register-ccv" placeholder="123">
+                                    <input type="text" class="form-control" name="add-ccv" id="add-ccv" placeholder="123">
                                 </div>
                             </div>
-                            <button type="submit" class="btn btn-primary btn-block mb-4 mt-4">Submit</button>                            
+                            <button type="submit" name="submit" class="btn btn-primary btn-block mb-4 mt-4">Submit</button>                            
                         </form>
                     </div>
                 </div>
--- view/admin/members.php
+++ view/admin/members.php
@@ -7,6 +7,65 @@
 	$username = "root";
 	$password = "";
 	$dbname = "omts_at51";
+
+if(isset($_POST["submit"])){
+	try {
+	    $rpassword = $_POST['add-password'];
+	    $rfname = $_POST['add-fname'];
+	    $rlname = $_POST['add-lname'];
+	    $rcity = $_POST['add-city'];
+	    $rStName = $_POST['add-streetname'];
+	    $rStNum = $_POST['add-streetnum'];
+	    $rState = $_POST['add-state'];
+	    $rzip = $_POST['add-zip'];
+	    $rphone = $_POST['add-phone'];
+	    $remail = $_POST['add-email'];
+	    $rcard = $_POST['add-card'];
+	    $rexp = $_POST['add-expiration'];
+	    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	    // set the PDO error mode to exception
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    /*The account number shouldnt be the primary key, the email should be. */
+	    $sql = "INSERT INTO Users 
+	    VALUES(DEFAULT, FALSE, '$rpassword', '$rfname','$rlname','$rStName',$rStNum,'$rcity','$rState', '$rzip', '$rphone','$remail', $rcard , '$rexp');";
+	    /*echo $sql; done for testing purposes */
+	    /*VAVLUES(Account no, isAdmin, password, fname, lname, str name, str num, city, state, zip, phone, email, card num, card expir)*/
+	    if ($conn->query($sql)) {
+	        //echo "Success";
+	        echo '<script type="text/javascript">alert("User was added!"); window.location="members.php"; </script>';
+	    }
+	    else{
+	        echo "error";
+	    }
+
+	    $dbh = null;
+	    }
+	catch(PDOException $e){
+	    echo "Connection failed: " . $e->getMessage();
+	}
+}
+if(isset($_POST['del_user'])){
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	    // set the PDO error mode to exception
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$sql = 'DELETE FROM Users WHERE id = '.$_POST['del_user'];  
+
+		if ($conn->query($sql)) {
+	        //echo "Success";
+	        echo '<script type="text/javascript">alert("User was deleted!"); window.location="members.php"; </script>';
+	    }
+	    else{
+	        echo "error";
+	    }
+
+	    $dbh = null;
+	    }
+	catch(PDOException $e){
+	    echo "Connection failed: " . $e->getMessage();
+	}
+}   
 ?>
 <h1>Members</h1>
 
@@ -15,6 +74,7 @@
 <table>
 	<thead>
 		<tr>
+			<th>ID</th>
 			<th>Name</th>
 			<th>Surname</th>
 			<th>Street</th>
@@ -30,7 +90,7 @@
 	  try{
 	    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-	    $sql = "SELECT fname, lname, street_name, street_number, city, state, zip FROM `users`;";
+	    $sql = "SELECT id, fname, lname, street_name, street_number, city, state, zip FROM `users`;";
 	    $sth = $conn->prepare($sql);
 	    
 	    if(!$sth->execute()) {
@@ -42,10 +102,14 @@
 	        foreach($row as $value) {
 	            echo "   <td>{$value}</td>   ";
 	        }
-	        echo '<td class="action-col">
-	        <a href="./scripts/adminRemoveMember?id='..'" title="Go to detail"><img class="icon-action" src="../../assets/img/detail.svg"></a>
-	        <a href="#" title="Delete row"><img class="icon-action" src="../../assets/img/delete.svg"></a>
-	        </td>';
+	        echo '<td class="action-col">';
+	        echo '<form action="" method="post"> 
+    				<input type="hidden" value="'.$row['id'].'" name="del_user" />                 
+    				<button type="submit" name="del_user-submit" class="no-style-btn"><img class="icon-action" src="../../assets/img/delete.svg"></button>
+				</form> 
+	        <!--<a href="#" title="Go to detail"></a>
+	        <a href="#" title="Delete row"><img class="icon-action" src="../../assets/img/delete.svg"></a>-->
+	        </td>';	       
 	        echo "</tr>";
 	    }
 	}
